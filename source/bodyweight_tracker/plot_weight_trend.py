1beec15fea1b97e0cd14df4b26cebdf8140cb969
Piotr Gałecki <herzlicheinladen@gmail.com>
2024-01-26 16:05:14 +0100

    Commited changes in plot_weight_trend.py

--- source/bodyweight_tracker/plot_weight_trend.py
+++ source/bodyweight_tracker/plot_weight_trend.py
@@ -1,3 +1,9 @@
 import pandas as pd
+
 import bokeh
-print("Placeholder")
+from definitions import INPUT_PATH
+
+input_file_path = INPUT_PATH.joinpath("weight_input.csv")
+print(input_file_path)
+
+df = pd.read_csv(input_file_path, parse_dates=[0], date_format="%d.%m.%y")
