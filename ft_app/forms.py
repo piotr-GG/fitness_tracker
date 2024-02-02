fac2e403fa75ad45cb5f9f144cb3c3cb5f070fe6
Piotr Gałecki <herzlicheinladen@gmail.com>
2024-02-02 13:06:27 +0100

    Added forms.py

---
+++ ft_app/forms.py
@@ -0,0 +1,11 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', validators=[validators.Length(min=4, max=25)])
+    email = StringField('Email Address', validators=[validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', validators= [
+        validators.DataRequired(),
+        validators.EqualTo('confirm_pass', message="Passwords must match!")
+    ])
+    confirm_pass = PasswordField('Repeat Password')
