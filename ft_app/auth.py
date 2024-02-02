3eaedcbd847d1cdba777ce7959e32f618611a55d
Piotr Gałecki <herzlicheinladen@gmail.com>
2024-02-02 13:22:40 +0100

    Added very rudimentary flashing option and implemented first form with WTForms

--- ft_app/auth.py
+++ ft_app/auth.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, flash, g, redirect, request, session, url_for, render_template
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from ft_app.forms import RegistrationForm
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @bp.route('/user_panel', methods=['GET', 'POST'])
 def user_panel():
     return render_template("auth/user_panel.html")
-
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -17,4 +18,8 @@ def login():
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("auth/register.html")
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
+        flash("Muchas gracias por registrarte!")
+        return redirect(url_for('auth.login'))
+    return render_template("auth/register.html", form=form)
--- ft_app/forms.py
+++ ft_app/forms.py
@@ -4,7 +4,7 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators
 class RegistrationForm(Form):
     username = StringField('Username', validators=[validators.Length(min=4, max=25)])
     email = StringField('Email Address', validators=[validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', validators= [
+    password = PasswordField('New Password', validators=[
         validators.DataRequired(),
         validators.EqualTo('confirm_pass', message="Passwords must match!")
     ])
