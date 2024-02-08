02399a4f4c73443d24647013144d1413cb8805e3
Piotr Gałecki <herzlicheinladen@gmail.com>
2024-02-08 16:22:38 +0100

    Changes to populating of DBC

--- ft_app/__init__.py
+++ ft_app/__init__.py
@@ -61,10 +61,15 @@ def populate_dbc():
         db_session.add(User(username="adrian", password="1234567", email="ceo@gmail.com", is_moderator=True))
         db_session.add(User(username="ganesh", password="ganesh", email="ganesh@gmail.com"))
 
-        db_session.add(BodyWeightRecord(weight=90.5, date="2023-01-01"))
-        db_session.add(BodyWeightRecord(weight=91.2, date="2023-01-02"))
-        db_session.add(BodyWeightRecord(weight=92.5, date="2023-01-03"))
-        db_session.add(BodyWeightRecord(weight=95.5,  date="2023-01-04"))
+        db_session.add(BodyWeightRecord(weight=90.5, date="2023-01-01", user_id=1))
+        db_session.add(BodyWeightRecord(weight=91.2, date="2023-01-02", user_id=1))
+        db_session.add(BodyWeightRecord(weight=92.5, date="2023-01-03", user_id=1))
+        db_session.add(BodyWeightRecord(weight=95.5, date="2023-01-04", user_id=1))
+
+        db_session.add(BodyWeightRecord(weight=68.5, date="2023-02-01", user_id=2))
+        db_session.add(BodyWeightRecord(weight=71.2, date="2023-02-02", user_id=2))
+        db_session.add(BodyWeightRecord(weight=65.5, date="2023-02-03", user_id=2))
+        db_session.add(BodyWeightRecord(weight=67.5, date="2023-02-04", user_id=2))
 
         db_session.add(BlogPost(date=datetime.datetime.utcnow(),
                                 title="yyyy, eee, yyy",
